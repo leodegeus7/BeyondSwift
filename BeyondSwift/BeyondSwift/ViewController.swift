ee75d4c988c551a24e2ee87543333bdab959a06b
Leonardo Geus <leonardodegeus@gmail.com>
2018-08-20 11:21:20 -0300

    First commit Added pod dependencies - RxSwift,RxCocoa

---
+++ BeyondSwift/BeyondSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeyondSwift
+//
+//  Created by Leonardo Geus on 20/08/2018.
+//  Copyright © 2018 Leonardo Geus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ BeyondSwift/Pods/Target Support Files/Pods-BeyondSwiftTests/Pods-BeyondSwiftTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BeyondSwiftTests : NSObject
+@end
+@implementation PodsDummy_Pods_BeyondSwiftTests
+@end
