fd9cd197847a9c3b179707556bfcbf076d4c0d48
Leonardo Geus <leonardodegeus@gmail.com>
2018-08-24 15:06:23 -0300

    Created Lint Created object mapper

---
+++ BeyondSwift/BeyondSwift/TestViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BeyondSwift
+//
+//  Created by Leonardo Geus on 20/08/2018.
+//  Copyright © 2018 Leonardo Geus. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    var viewModel:TestViewModel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let hasWarning :String = ""
+        print(hasWarning)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
